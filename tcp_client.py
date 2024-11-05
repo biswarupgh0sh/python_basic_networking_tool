1f4f3658b3af7844dc146620cdb3cd8a92400874
Biswarup <biswarupghosh520@gmail.com>
2024-11-05 23:21:48 +0530

    initial commit

---
+++ tcp_client.py
@@ -0,0 +1,27 @@
+import socket
+
+#mentioning the target host
+target_host="www.example.org" # expecting to change it when using according to need
+
+#process the host to break into arrays divided when it see a dot  
+processing_host = target_host.split(".")
+
+# create the host without the (www) or (http://www) or (https://www) part
+host = ".".join(processing_host[1:])
+# mentioning the target port
+target_port=80 # expecting to change it when using according to need
+
+# create a socket object where AF_INET which accepts ipv4 or hostname and SOCK_STREAM which indicates that this will be a tcp client
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# connect the client
+client.connect((target_host, target_port))
+
+#send some data
+client.send(b"GET / HTTP/1.1\r\nHOST: {}\r\n\r\n".format(host))
+
+#receive some data
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
---
+++ udp_client.py
@@ -0,0 +1,17 @@
+import socket
+
+target_host = "127.0.0.1" # expecting to change it when using according to need
+
+target_port = 5000 # expecting to change it when using according to need
+
+# create a socket object where AF_INET which accepts ipv4 or hostname and SOCK_DGRAM which indicates that this will be a udp client
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#since udp is a connectionless protocol it doesn't need to establish a connection before sending the data, it needs host and port only when sending the data
+client.sendto(b"It is a udp client", (target_host, target_port))
+
+# receive data
+data, addr = client.recvfrom(4096)
+
+print(data.decode())
+client.close()
